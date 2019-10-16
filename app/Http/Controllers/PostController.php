c90e76a20cc54daebbee3c9f7a89171edf655d38
irerimwenda <bmugendi98@gmail.com>
2019-10-16 13:20:14 +0300

    publishing post to db

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+
+use App\Post;
 
 class PostController extends Controller
 {
     //store post
     public function store(Request $request) {
-        return $request->all();
+        //return $request->all();
+
+        return Post::create([
+                'user_id' => Auth::id(),
+                'content' => $request->content
+            ]);
     }
 }
